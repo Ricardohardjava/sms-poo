836f7d58cab9568afceac2ed6af9b938addb6688
Ricardo <ricardoalbuquerquecorretor@gmail.com>
2022-02-28 15:31:46 -0300

    initial commit

---
+++ src/principal/main.java
@@ -0,0 +1,12 @@
+package principal;
+
+import sistema.Sistema;
+
+class Main {
+
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        sistema.inicializar();
+
+    }
+}
